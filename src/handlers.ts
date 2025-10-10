ba4b6c4658d377222ae3f85e1495cd8afe6ee39b
Damien Leroy <dleroy@smad.be>
2025-10-10 11:04:53 +0200

    use a single handler for both WS and rest http requests

---
+++ src/handlers.ts
@@ -0,0 +1,60 @@
+import { ALBEvent, APIGatewayProxyEvent, Context } from 'aws-lambda';
+import createAPI, { Request, Response } from 'lambda-api';
+import createWsServer from './utils/lambda-ws-server';
+import { forumRoutes, forumWsActions } from './forum/routes';
+import errorHandlingMiddleware from './middlewares/error-handling';
+import corsMiddleware from './middlewares/cors';
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// HTTP REST handlers
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const api = createAPI({
+  base: 'sls',
+  logger: true,
+});
+
+// middlewares
+api.use(errorHandlingMiddleware);
+api.use(corsMiddleware);
+
+// OPTION handling (cors headers are injected by the middleware)
+api.options('/*', (_req: Request, res: Response) => {
+  res.status(200).send({});
+});
+
+// routes registration
+api.register(forumRoutes, { prefix: '/forum' });
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// WEBSOCKET handlers
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const wsServer = createWsServer();
+
+wsServer.register(forumWsActions, { prefix: 'forum' });
+wsServer.on('heartbeat', () => {});
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// GLOBAL handler
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Global handler for HTTP REST and WEBSOCKET requests.
+ * `event` is of type `APIGatewayProxyHandler` while it may be an "ALBEvent" in practice... but the lamda-API
+ * lib expects a `APIGatewayProxyHandler` even in this case.
+ */
+export async function globalHandler(event: APIGatewayProxyEvent|ALBEvent, context: Context): Promise<unknown> {
+  if (event.httpMethod) {
+    // event is a ALBEvent. But the lambda-API lib expects a `APIGatewayProxyHandler`
+    return api.run(event as APIGatewayProxyEvent, context);
+
+  } else if ('eventType' in event.requestContext) {
+    return wsServer.handler(event as APIGatewayProxyEvent, context);
+
+  } else {
+    throw new Error('event not supported');
+  }
+}
